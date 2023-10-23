59fa9a76312cad17acc33f82f631461a06ae754b
alinr10 <98055702+alinr10@users.noreply.github.com>
2023-10-23 22:12:34 +0300

    api added

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const btnElList = document.querySelectorAll('.btn');
 
-btnElList.forEach(btnEl =>{
-    btnEl.addEventListener('click',()=>{
+btnElList.forEach(btnEl => {
+    btnEl.addEventListener('click', () => {
         document.querySelector('.special')?.classList.remove('special');
         btnEl.classList.add('special');
     });
 });
+
