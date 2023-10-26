23661f01a5cd874001d062d7beea2aa29858667e
Eren <erenkeskin07@gmail.com>
2023-10-26 17:51:03 +0300

    fix:app.js index.css league-table.ejs

--- app.js
+++ app.js
@@ -2,11 +2,44 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const { render } = require('ejs');
+const axios = require('axios');
 const app = express();
 app.use(express.static("public"));
+
 dotenv.config();
 
+const teams=[];
 
+
+app.get('/league-table', async (req, res) => {
+  try {
+    const response1 = await axios.get('https://api.football-data.org/v4/competitions/PL/standings',{
+        headers:{
+            'X-Auth-Token': 'bf22ceb1e3144616822aaa16640551cd'
+        }
+    });
+    const data1 = response1.data;
+    const standings = data1.standings[0].table;
+
+    const response2 = await axios.get('https://football-web-pages1.p.rapidapi.com/form-guide.json',{
+        params:{
+        comp:'1',
+        team:'1'
+  },
+  headers:{
+        'X-RapidAPI-Key': 'caa1494095msh51251f309697f53p1607c8jsneb6421b7c32b',
+        'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
+  }
+});
+    const data2 = response2.data;
+    const matches=data2['form-guide'].teams[0].matches;
+
+    res.render('league-table.ejs', { standings});
+  } catch (error) {
+    console.error('API İsteği Sırasında Hata:', error);
+    res.status(500).send('API isteği sırasında hata oluştu.');
+  }
+});
 
 
 async function get_matchday_matches(matchday) {
@@ -44,11 +77,6 @@ app.get('/',async (req,res)=>{
     res.render("index.ejs", { matches });
 
 })
-
-app.get('/league-table',async (req,res)=>{
-    res.render("league-table.ejs");
-})
-
 
 
 const PORT = process.env.PORT || 5000;
