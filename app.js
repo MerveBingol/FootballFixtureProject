cbf8740b02f123f17f9924a14e94b8fc2ec11dde
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2023-10-27 12:27:48 +0300

    fix:logo and merged dev

--- app.js
+++ app.js
@@ -12,74 +12,72 @@ const teams=[];
 
 
 app.get('/league-table', async (req, res) => {
-  try {
-    const response1 = await axios.get('https://api.football-data.org/v4/competitions/PL/standings',{
-        headers:{
-            'X-Auth-Token': 'bf22ceb1e3144616822aaa16640551cd'
-        }
-    });
-    const data1 = response1.data;
-    const standings = data1.standings[0].table;
-
-    const response2 = await axios.get('https://football-web-pages1.p.rapidapi.com/form-guide.json',{
-        params:{
-        comp:'1',
-        team:'1'
-  },
-  headers:{
-        'X-RapidAPI-Key': 'caa1494095msh51251f309697f53p1607c8jsneb6421b7c32b',
-        'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
-  }
-});
-    const data2 = response2.data;
-    const matches=data2['form-guide'].teams[0].matches;
-
-    res.render('league-table.ejs', { standings});
-  } catch (error) {
-    console.error('API İsteği Sırasında Hata:', error);
-    res.status(500).send('API isteği sırasında hata oluştu.');
-  }
-});
-
-
-app.get('/conf',async (req,res)=>{
-    let livedata = await fetch('https://livescore-api.com/api-client/scores/live.json?competition_id=2&key=zIFEF40GZSg5tGZK&secret=Ll89Hz5AS1yao1OXKtCpyD6yvmDY2DJO')
-    .then(response => response.json())
-    .catch(err => console.error(err));
+    try {
+      const response1 = await axios.get('https://api.football-data.org/v4/competitions/PL/standings',{
+          headers:{
+              'X-Auth-Token': 'bf22ceb1e3144616822aaa16640551cd'
+          }
+      });
+      const data1 = response1.data;
+      const standings = data1.standings[0].table;
   
-    console.log(livedata['data']['match'][0])
-
-    //res.render("index.ejs", { matches });
-})
-
-
-
-
-app.get('/index',async (req,res)=>{
-
-    let matches = await fetch('https://api.football-data.org/v4/competitions/PL/matches', {
-        headers: {
-            method: 'GET',
-            'X-Auth-Token': '28185993d8f54afd9cdb5a945a155849',
-        },
-    })
-        .then(response => response.json())
-        // .then(data => console.log(data))
-        .catch(err => console.error(err));
-
-      
+      const response2 = await axios.get('https://football-web-pages1.p.rapidapi.com/form-guide.json',{
+          params:{
+          comp:'1',
+          team:'1'
+    },
+    headers:{
+          'X-RapidAPI-Key': 'caa1494095msh51251f309697f53p1607c8jsneb6421b7c32b',
+          'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
+    }
+  });
+      const data2 = response2.data;
+      const matches=data2['form-guide'].teams[0].matches;
   
-    matches = matches['matches'];
-
-    res.render("index.ejs", { matches });
-
-})
-
-
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log("server is running on:5000");
-})
+      res.render('league-table.ejs', { standings});
+    } catch (error) {
+      console.error('API İsteği Sırasında Hata:', error);
+      res.status(500).send('API isteği sırasında hata oluştu.');
+    }
+  });
+  
+  
+  app.get('/conf',async (req,res)=>{
+      let livedata = await fetch('https://livescore-api.com/api-client/scores/live.json?competition_id=2&key=zIFEF40GZSg5tGZK&secret=Ll89Hz5AS1yao1OXKtCpyD6yvmDY2DJO')
+      .then(response => response.json())
+      .catch(err => console.error(err));
+    
+      console.log(livedata['data']['match'][0])
+  
+      //res.render("index.ejs", { matches });
+  })
+  
+  
+  
+  
+  app.get('/index',async (req,res)=>{
+  
+      let matches = await fetch('https://api.football-data.org/v4/competitions/PL/matches', {
+          headers: {
+              method: 'GET',
+              'X-Auth-Token': '28185993d8f54afd9cdb5a945a155849',
+          },
+      })
+          .then(response => response.json())
+          // .then(data => console.log(data))
+          .catch(err => console.error(err));
+  
+        
+    
+      matches = matches['matches'];
+  
+      res.render("index.ejs", { matches });
+  
+  })
+  
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.listen(PORT, () => {
+      console.log("server is running on:5000");
+  })
